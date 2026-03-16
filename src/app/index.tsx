215fa5198d21ccaa5616ce292f0174cf6a8bc1bc
Javier Granados <hola@javiergranados.dev>
2026-03-16 23:30:59 +0100

    install nativewind

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,17 +1,9 @@
-import { StyleSheet, Text, View } from "react-native";
+import { Text, View } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Navigation App</Text>
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-4xl text-blue-500 font-bold">Navigation App</Text>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
